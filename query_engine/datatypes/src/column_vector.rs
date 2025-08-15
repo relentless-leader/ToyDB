6dd43bdecdce1bb25237f0e0499002c8ebc16ce1
Rishabh Bhatia <bhatiarishabh63@gmail.com>
2025-08-15 06:53:37 -0700

    checkpointing the datatypes logic. Will move on use Arrow Schema, datatype and record batch for now. Will refactor later on if I want a custom types.

--- query_engine/datatypes/src/column_vector.rs
+++ query_engine/datatypes/src/column_vector.rs
@@ -1,38 +1,41 @@
-trait ColumnVector<T> {
-    fn get_type(&self) -> ArrowTypes;
-    fn get_value(&self, i: usize) -> T;
+use crate::arrow_types::ArrowTypes;
+use std::any::Any;
+
+pub trait ColumnVector {
+    fn get_type(&self) -> &ArrowTypes;
+    fn get_value(&self, i: usize) -> Option<&dyn Any>;
     fn size(&self) -> usize;
 }
 
-struct LiteralValueVector<T: Clone> {
+pub struct LiteralValueVector {
     arrow_type: ArrowTypes,
-    value: T,
+    value: Option<Box<dyn Any>>,
     size: usize,
 }
 
-impl<T:Clone> LiteralValueVector<T> {
-    pub fn new(arrow_type:ArrowTypes, value: T, size: usize) -> Self {
+impl LiteralValueVector {
+    pub fn new(arrow_type: ArrowTypes, value: Option<Box<dyn Any>>, size: usize) -> Self {
         Self {
             arrow_type,
             value,
-            usize,
+            size,
         }
     }
 }
 
-impl<T: Clone> ColumnVector<T> for LiteralValueVector<T> {
-    fn get_type(&self) -> ArrowTypes {
-        self.arrow_type.clone()
+impl ColumnVector for LiteralValueVector {
+    fn get_type(&self) -> &ArrowTypes {
+        &self.arrow_type
     }
 
-    fn get_value(&self, i) -> T {
-        if i >= self.size {
+    fn get_value(&self, i: usize) -> Option<&dyn Any> {
+        if i < 0 || i >= self.size {
             panic!("Index out of bound: {}", i);
         }
-        self.value.clone()
+        self.value.as_deref()
     }
 
-    fn size(&self) {
+    fn size(&self) -> usize {
         self.size
     }
 }
---
+++ query_engine/datatypes/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod arrow_types;
+pub mod column_vector;
+
+pub use arrow_types::ArrowTypes;
+pub use column_vector::ColumnVector;
+pub use column_vector::LiteralValueVector;
